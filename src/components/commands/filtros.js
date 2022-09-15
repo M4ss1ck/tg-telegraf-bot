8c127dedbb4ee31996fa10d7c6edea5f2a404713
Massick <4ndyraul@gmail.com>
2022-09-15 18:25:57 -0400

    add ability to retrieve filters

--- src/components/commands/filtros.js
+++ src/components/commands/filtros.js
@@ -94,4 +94,131 @@ filtros.command(["filters", "filtros"], async (ctx) => {
   ctx.replyWithHTML("<b>Lista de filtros:</b>\n" + filtrosTexto);
 });
 
+filtros.on("message", async (ctx) => {
+  const chatId = ctx.chat.id.toString() ?? "global";
+  // get filter for that specific chat
+  const filters = await prisma.filtro.findMany({
+    where: {
+      chat: chatId,
+    },
+  });
+
+  filters.length > 0 &&
+    filters.some((filter) => {
+      const regex = new RegExp("^" + filter.filtro + "$", "i");
+      if (
+        (ctx.message.text && ctx.message.text.match(regex)) ||
+        (ctx.message.caption && ctx.message.caption.match(regex))
+      ) {
+        const respuesta = JSON.parse(filter.respuesta);
+        const caption = respuesta.caption ?? null;
+        const markup = respuesta.reply_markup ?? null;
+
+        console.log(respuesta);
+        if (filter.tipo === "text") {
+          const entities = respuesta.entities || [];
+          console.log("Entities:\n", entities);
+          let texto_final = respuesta.text;
+          entities.map((entity) => {
+            const { offset, length, type } = entity;
+            let tag;
+            switch (type) {
+              case "text_link":
+                tag = "a";
+                break;
+              case "bold":
+                tag = "b";
+                break;
+              case "italic":
+                tag = "i";
+                break;
+              case "code":
+                tag = "code";
+                break;
+              case "pre":
+                tag = "pre";
+                break;
+              case "text_mention":
+                tag = "a";
+                break;
+              case "strikethrough":
+                tag = "s";
+                break;
+              case "underline":
+                tag = "u";
+                break;
+              default:
+                tag = "i";
+                break;
+            }
+            console.log("Tag ", tag);
+            texto_final = texto_final.replace(
+              respuesta.text.substr(offset, length),
+              `<${tag}${
+                entity.url ? ` href="${entity.url}"` : ``
+              }>${respuesta.text.substr(offset, length)}</${tag}>`
+            );
+          });
+
+          ctx.replyWithHTML(texto_final, {
+            reply_to_message_id: ctx.message.message_id,
+            reply_markup: markup,
+          });
+        } else if (filter.tipo === "photo") {
+          ctx
+            .replyWithPhoto(
+              respuesta.photo[respuesta.photo.length - 1].file_id,
+              {
+                caption: caption,
+                reply_to_message_id: ctx.message.message_id,
+                reply_markup: markup,
+              }
+            )
+            .catch((err) => ctx.reply(JSON.stringify(err)));
+        } else if (filter.tipo === "sticker") {
+          ctx
+            .replyWithSticker(respuesta.sticker.file_id, {
+              reply_to_message_id: ctx.message.message_id,
+              reply_markup: markup,
+            })
+            .catch((err) => ctx.reply(JSON.stringify(err)));
+        } else if (filter.tipo === "voice") {
+          ctx
+            .replyWithVoice(respuesta.voice.file_id, {
+              caption: caption,
+              reply_to_message_id: ctx.message.message_id,
+              reply_markup: markup,
+            })
+            .catch((err) => ctx.reply(JSON.stringify(err)));
+        } else if (filter.tipo === "video") {
+          ctx
+            .replyWithVideo(respuesta.video.file_id, {
+              caption: caption,
+              reply_to_message_id: ctx.message.message_id,
+              reply_markup: markup,
+            })
+            .catch((err) => ctx.reply(JSON.stringify(err)));
+        } else if (filter.tipo === "audio") {
+          ctx
+            .replyWithAudio(respuesta.audio.file_id, {
+              caption: caption,
+              reply_to_message_id: ctx.message.message_id,
+              reply_markup: markup,
+            })
+            .catch((err) => ctx.reply(JSON.stringify(err)));
+        } else {
+          ctx
+            .replyWithDocument(respuesta.document.file_id, {
+              caption: caption,
+              reply_to_message_id: ctx.message.message_id,
+              reply_markup: markup,
+            })
+            .catch((err) => ctx.reply(JSON.stringify(err)));
+        }
+        return true;
+      }
+      return false;
+    });
+});
+
 export default filtros;
