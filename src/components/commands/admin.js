34b1c782a9b69ae0b00b57d57587908462f11633
Massick <4ndyraul@gmail.com>
2022-09-20 16:09:02 -0400

    fix admin commands

---
+++ src/components/commands/admin.js
@@ -0,0 +1,29 @@
+import { Composer } from "telegraf";
+
+const admin = new Composer();
+
+const my_id = process.env.ADMIN_ID;
+
+admin.command(
+  "quit",
+  Composer.acl(parseInt(my_id), (ctx) => {
+    if (ctx.message.from.id.toString() == my_id) {
+      ctx
+        .reply("Me fui 👋")
+        .then(() =>
+          ctx.chat.type !== "private"
+            ? ctx.telegram.leaveChat(ctx.message.chat.id)
+            : ctx.reply("Era jugando")
+        );
+    }
+  })
+);
+
+admin.command(
+  "admin",
+  Composer.acl(parseInt(my_id), (ctx) => {
+    ctx.reply("Eres administrador de este bot");
+  })
+);
+
+export default admin;
--- src/main.js
+++ src/main.js
@@ -8,11 +8,13 @@ import love from "./components/commands/love.js";
 import inline from "./components/inline/inline.js";
 import replacer from "./components/commands/replace.js";
 import polls from "./components/commands/polls.js";
+import admin from "./components/commands/admin.js";
 // import axios from "axios";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot
+  .use(admin)
   .use(actions)
   .use(commands)
   .use(reputation)
