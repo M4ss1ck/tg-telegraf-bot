f801bab7d3c348710762155a1ba4f4f6631e39cc
Massick <4ndyraul@gmail.com>
2022-09-20 09:07:12 -0400

    move replacer out of main.js

---
+++ src/components/commands/replace.js
@@ -0,0 +1,48 @@
+import { Composer, Markup } from "telegraf";
+import { prisma } from "../db/prisma.js";
+
+const replacer = new Composer();
+
+replacer.hears(/^\/(s|s@\w+)(\/)?$/i, (ctx) =>
+  ctx.replyWithHTML(
+    `Debe escoger qué parte del mensaje desea reemplazar y con qué desea hacerlo.\nPor ejemplo, si tenemos un mensaje que diga "Eres feo" y queremos transformarlo en "Eres hermoso", debemos usar <code>/s/feo/hermoso</code> respondiendo dicho mensaje.\n\n<b>Nota:</b> Si el bot es administrador, borrará nuestro mensaje`,
+    {
+      reply_to_message_id: ctx.message.message_id,
+    }
+  )
+);
+
+replacer.hears(/^\/(s|s@\w+)\/(.+)?\/(.+)?/i, (ctx) => {
+  let [, , search, replace] = ctx.match;
+  replace = replace ?? "";
+  console.log(search, replace);
+  let text = "";
+  if (ctx.message.reply_to_message) {
+    let msg =
+      ctx.message.reply_to_message.text ?? ctx.message.reply_to_message.caption;
+    msg = msg.replace('En realidad quisiste decir: \n\n"', "");
+    text =
+      '<b>En realidad quisiste decir:</b> \n\n"' +
+      msg.replace(new RegExp(search, "g"), replace) +
+      '"';
+    ctx
+      .replyWithHTML(text, {
+        reply_to_message_id: ctx.message.reply_to_message.message_id,
+      })
+      .then(() => {
+        ctx.deleteMessage().catch(() => {
+          console.log("No se pudo borrar el mensaje");
+          const keyboard = Markup.inlineKeyboard([
+            [Markup.button.callback("Borrar", "del")],
+          ]);
+          ctx.replyWithHTML("No pude borrar el mensaje", keyboard);
+        });
+      });
+  } else {
+    ctx.reply("Debes responder un mensaje o de lo contrario no funcionará", {
+      reply_to_message_id: ctx.message.message_id,
+    });
+  }
+});
+
+export default replacer;
