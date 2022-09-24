c6f185f75fe7c17b6f0b9c052a6290e77369da87
Massick <4ndyraul@gmail.com>
2022-09-24 15:17:35 -0400

    rework elapsedTime()

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -67,26 +67,30 @@ function adornarRango(rango: string) {
   return rangoAdornado
 }
 
+const padTo2Digits = (num: number) => {
+  return num.toString().padStart(2, '0')
+}
+
+const convertMsToTime = (milliseconds: number) => {
+  let seconds = Math.floor(milliseconds / 1000)
+  let minutes = Math.floor(seconds / 60)
+  let hours = Math.floor(minutes / 60)
+  const days = Math.floor(hours / 24)
+
+  seconds = seconds % 60
+  minutes = minutes % 60
+  hours = hours % 24
+
+  return `${days}:${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
+    seconds,
+  )}`
+}
+
 // calcular el tiempo que tarda el bot en arrancar
 const elapsedTime = (inicio: number) => {
   const ahora = performance.now()
   const activo = ahora - inicio
-  // dar el resultado en dependencia del tiempo
-  if (activo > 60 * 60 * 1000) {
-    const valor = roundToAny(activo / 3600000, 2)
-    const horas = Math.floor(valor)
-    const minutos = roundToAny((valor - horas) * 60, 0)
-    return `${horas} h ${minutos} min`
-  }
-  else if (activo > 60000) {
-    const valor = roundToAny(activo / 60000, 2)
-    const minutos = Math.floor(valor)
-    const segundos = roundToAny((valor - minutos) * 60, 0)
-    return `${minutos} min ${segundos} s`
-  }
-  else {
-    return `${roundToAny(activo / 1000, 1)} s`
-  }
+  return convertMsToTime(activo)
 }
 
 const timeToNext = (time: number) => {
