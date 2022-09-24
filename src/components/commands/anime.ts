0ac7d5d7b0164b76037556da2b6445bb1c4b58ee
Massick <4ndyraul@gmail.com>
2022-09-23 22:36:51 -0400

    add pagination

--- src/components/commands/anime.ts
+++ src/components/commands/anime.ts
@@ -59,11 +59,18 @@ anime.command('anime', async (ctx) => {
     // buscar en AniList
     const results = await getAnimes(search)
     const media = results.Page?.media as Anime[]
+    const total = results.Page?.pageInfo?.total as number ?? 1
+    const perPage = results.Page?.pageInfo?.perPage as number ?? 5
     if (media && media.length > 0) {
       const buttons = []
       for (const anime of media)
         buttons.push([Markup.button.callback(anime.title.romaji ?? 'placeholder text', `getAnime${anime.id}`)])
 
+      buttons.push([
+        Markup.button.callback('⏭', `AnimPage${2}-${search}`, total / perPage <= 1),
+      ])
+
+      console.log(total, perPage, total / perPage > 1)
       const keyboard = Markup.inlineKeyboard(buttons)
       const text = `Resultados para <b>${search}</b>`
 
@@ -71,6 +78,39 @@ anime.command('anime', async (ctx) => {
     }
     else {
       ctx.replyWithHTML('No se encontraron resultados o hubo un error')
+    }
+  }
+})
+
+anime.action(/AnimPage\d+\-/i, async (ctx) => {
+  const pageString = ctx.callbackQuery.data?.match(/AnimPage(\d+)/i)?.[1]
+  // console.log(ctx.callbackQuery.data?.match(/AnimPage(\d+)/i))
+  const page = parseInt(pageString ?? '1')
+  console.log('pagina actual ', page)
+  const search = ctx.callbackQuery.data?.replace(/AnimPage\d+\-/i, '')
+  if (search && search.length > 2) {
+    // buscar en AniList
+    const results = await getAnimes(search, page)
+    const media = results.Page?.media as Anime[]
+    const total = results.Page?.pageInfo?.total as number ?? 1
+    const perPage = results.Page?.pageInfo?.perPage as number ?? 5
+    if (media && media.length > 0) {
+      const buttons = []
+      for (const anime of media)
+        buttons.push([Markup.button.callback(anime.title.romaji ?? 'placeholder text', `getAnime${anime.id}`)])
+
+      const showPrevBtn = page >= 2
+      const showNextBtn = total / perPage > page
+
+      const lastRow = []
+      showPrevBtn && lastRow.push(Markup.button.callback('⏮', `AnimPage${page - 1}-${search}`))
+      showNextBtn && lastRow.push(Markup.button.callback('⏭', `AnimPage${page + 1}-${search}`))
+
+      buttons.push(lastRow)
+
+      ctx.editMessageReplyMarkup({
+        inline_keyboard: buttons,
+      })
     }
   }
 })
@@ -121,16 +161,54 @@ anime.command('character', async (ctx) => {
   if (search.length > 2) {
     const results = await getCharacters(search)
     const characters = results.Page?.characters as Character[]
+    const total = results.Page?.pageInfo?.total as number ?? 1
+    const perPage = results.Page?.pageInfo?.perPage as number ?? 5
 
     if (characters && characters.length > 0) {
       const buttons = []
       for (const char of characters)
         buttons.push([Markup.button.callback(char.name.full ?? 'error con el nombre', `getCharacter${char.id}`)])
 
+      buttons.push([
+        Markup.button.callback('⏭', `CharPage${2}-${search}`, total / perPage <= 1),
+      ])
+
       const keyboard = Markup.inlineKeyboard(buttons)
       const text = `Resultados para <i>${search}</i>`
 
       ctx.replyWithHTML(text, keyboard)
+    }
+  }
+})
+
+anime.action(/CharPage\d+\-/i, async (ctx) => {
+  const pageString = ctx.callbackQuery.data?.match(/CharPage(\d+)/i)?.[1]
+  const page = parseInt(pageString ?? '1')
+  console.log('editando el mensaje', page)
+  const search = ctx.callbackQuery.data?.replace(/CharPage\d+\-/i, '')
+  if (search && search.length > 2) {
+    const results = await getCharacters(search, page)
+    const characters = results.Page?.characters as Character[]
+    const total = results.Page?.pageInfo?.total as number ?? 1
+    const perPage = results.Page?.pageInfo?.perPage as number ?? 5
+
+    if (characters && characters.length > 0) {
+      const buttons = []
+      for (const char of characters)
+        buttons.push([Markup.button.callback(char.name.full ?? 'error con el nombre', `getCharacter${char.id}`)])
+
+      const showPrevBtn = page >= 2
+      const showNextBtn = total / perPage > page
+
+      const lastRow = []
+      showPrevBtn && lastRow.push(Markup.button.callback('⏮', `CharPage${page - 1}-${search}`))
+      showNextBtn && lastRow.push(Markup.button.callback('⏭', `CharPage${page + 1}-${search}`))
+
+      buttons.push(lastRow)
+
+      ctx.editMessageReplyMarkup({
+        inline_keyboard: buttons,
+      })
     }
   }
 })
