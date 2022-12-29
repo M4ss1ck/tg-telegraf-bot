46e938435270c12cc74f5586a5f37cead1c1ade6
Massick <4ndyraul@gmail.com>
2022-12-29 17:12:54 -0500

    change user list format

--- src/components/commands/admin.ts
+++ src/components/commands/admin.ts
@@ -29,8 +29,8 @@ admin.command(
 admin.command(
   'users',
   Composer.acl(parseInt(my_id), (ctx) => {
-    const users = `${JSON.stringify(global.USUARIOS, null, 2)}`
-    ctx.replyWithHTML(`<pre>${users.slice(0, 2037)}</pre>`)
+    const users = Object.values(global.USUARIOS).map(user => `[${user.rep}] ${user.nick} (${user.tg_id})`)
+    ctx.replyWithHTML(`<pre>${JSON.stringify(users, null, 2).slice(0, 2037)}</pre>`)
   }),
 )
 
