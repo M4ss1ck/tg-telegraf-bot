c788de3f4a3320fa522452d88027f1815be9cc00
Massick <4ndyraul@gmail.com>
2022-12-29 23:39:57 -0500

    fix /ud parsing

--- src/components/commands/ud.ts
+++ src/components/commands/ud.ts
@@ -3,20 +3,30 @@ import { search } from 'urban-dictionary-client'
 
 const urban = new Composer()
 
+const escape = (s: string) => s.replace(/\[|\]|<|>/g, '')
+
 // Urban Dictionary
 urban.command('ud', async (ctx) => {
   const query = ctx.message.text.substring(4)
   try {
     search(query)
       .then((res: any) => {
-        console.log(res)
         if (res.list) {
           const results = res.list as any[]
-          const text = results.map(res => `${res.definition}\n${res.example ?? 'No example found'}`).join('\n\n')
-          ctx.replyWithHTML(text.slice(0, 2048))
+          let text = `<b>Results for <i>"${query}"</i>:</b>`
+          for (let i = 0; i < results.length; i++) {
+            const res = results[i]
+            if (text.length + res.definition.length + res.example.length > 2035)
+              break
+
+            text += `\n\n${escape(res.definition)}\n<i>${res.example ? escape(res.example) : 'No example found'}</i>`
+          }
+          ctx.replyWithHTML(text).catch((err: any) => console.error(err))
         }
       })
-      .catch((err: any) => console.error(err))
+      .catch(() => ctx
+        .replyWithHTML('<b>Error with request!</b> Please try again.')
+        .catch((err: any) => console.error(err)))
   }
   catch (error) {
     console.log(error)
