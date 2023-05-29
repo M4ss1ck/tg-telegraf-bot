edba87ad208edf5ddb629d62c54a42cc89bc2361
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-05-29 13:25:46 -0400

    add i18n to ban composer

--- src/components/commands/ban.ts
+++ src/components/commands/ban.ts
@@ -1,14 +1,15 @@
 import { Composer } from 'telegraf'
+import type { MyContext } from '../../interfaces'
 
-const ban = new Composer()
+const ban = new Composer<MyContext>()
 
 ban.command('b', Composer.admin(async (ctx) => {
   if (ctx.message.reply_to_message?.from) {
     const bannedId = ctx.message.reply_to_message.from.id
     await ctx
       .banChatMember(bannedId)
-      .then(() => ctx.reply('Usuario baneado'))
-      .catch(() => ctx.reply('Error kicking user'))
+      .then(() => ctx.reply(ctx.t('Usuario baneado')!))
+      .catch(() => ctx.reply(ctx.t('Error kicking user')!))
   }
 }))
 
@@ -18,7 +19,7 @@ ban.command('k', Composer.admin(async (ctx) => {
     await ctx
       .banChatMember(bannedId)
       .then(() => ctx.unbanChatMember(bannedId))
-      .catch(() => ctx.reply('Error kicking user'))
+      .catch(() => ctx.reply(ctx.t('Error kicking user')!))
   }
 }))
 
