6ceaaa979e363ffaccec986accb0cc810991caf2
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-09-10 01:37:07 -0400

    adjust logic for i18n middleware

--- src/components/middleware/i18n.ts
+++ src/components/middleware/i18n.ts
@@ -1,21 +1,29 @@
 import { Composer } from 'telegraf'
 import type { MyContext } from '../../interfaces.js'
 import { i18n as i18next } from '../../locales/i18n.js'
+import { updateUser } from '../global/data.js'
 
 const i18n = new Composer<MyContext>()
 
 i18n.use(async (ctx, next) => {
-  if (
-    ctx.from?.id
-  ) {
-    const userLocale = ctx.session?.lang ?? ctx.from.language_code
-    const _i18next = i18next.cloneInstance({ initImmediate: false, lng: userLocale })
-    ctx.session = { lang: userLocale }
-    _i18next.on('languageChanged', (lng) => {
-      ctx.session.lang = lng
-    })
-    ctx.i18next = _i18next
-    ctx.t = _i18next.t
+  try {
+    if (
+      ctx.from?.id
+    ) {
+      const userObjFromGlobal = global.USUARIOS[ctx.from.id.toString()]
+      const userLocale = userObjFromGlobal?.lang ?? ctx.session?.lang ?? ctx.from.language_code
+      const _i18next = i18next.cloneInstance({ initImmediate: false, lng: userLocale })
+      ctx.session = { lang: userLocale }
+      _i18next.on('languageChanged', async (lng) => {
+        ctx.session.lang = lng
+        await updateUser({ ...userObjFromGlobal, lang: lng })
+      })
+      ctx.i18next = _i18next
+      ctx.t = _i18next.t
+    }
+  } catch (error) {
+    console.log('Error in i18n middleware')
+    console.log(error)
   }
   return next()
 })
